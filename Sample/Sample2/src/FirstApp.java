37a666162840ecdc3b3f92e8a9ecd471188a186e
kymx1983 <kymx1983@gmail.com>
2020-05-16 11:32:54 +0900

    2.1 Java基本構文.md作成

---
+++ Sample/Sample2/src/FirstApp.java
@@ -0,0 +1,5 @@
+public class FirstApp {
+  public static void main(String[] args) {
+    System.out.println("こんにちは。私の名前は●●です。");
+  }
+}
---
+++ Sample/Sample2/src/HelloTest2.java
@@ -0,0 +1,7 @@
+public class HelloTest2 {
+  public static void main(String[] args) {
+    System.out.println("こんにちは");
+    System.out.println("私の名前は");
+    System.out.println("●●です。");
+  }
+}
