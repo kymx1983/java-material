650ad6d20bdaa402668e1b4f825174b4671b9e36
kymx1983 <kymx1983@gmail.com>
2020-05-16 12:02:00 +0900

    2.2 計算処理作成

---
+++ Sample/Sample2/src/CalculationTest1.java
@@ -0,0 +1,8 @@
+public class CalculationTest1 {
+  public static void main(String[] args) {
+    System.out.println(1 + 1);
+    System.out.println(10 - 9);
+    System.out.println(9 * 9);
+    System.out.println(100 / 5);
+  }
+}
---
+++ Sample/Sample2/src/CalculationTest2.java
@@ -0,0 +1,5 @@
+public class CalculationTest2 {
+  public static void main(String[] args) {
+    System.out.println(100 % 9);
+  }
+}
