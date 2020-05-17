f1bed97e490382e2bae54876000c3525ffc80ec1
kymx1983 <kymx1983@gmail.com>
2020-05-17 17:08:23 +0900

    2.3 変数.md作成

--- Sample/Sample2/src/Circle.java
+++ Sample/Sample2/src/Circle.java
@@ -1,9 +1,9 @@
 public class Circle {
   public static void main(String[] args) {
-    int radius = 5;
+    int radius = 15;
     final double pi = 3.14;
 
-    double result = radius * radius * pi;
+    double result = 2 * radius * pi;
 
     System.out.println("半径" + radius + "の場合の円の面積は" + result + "です。");
   }
